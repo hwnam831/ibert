6d3e483d8b1b14da457ab103f6bc5ae308445b28
Vikram <vsm2@illinois.edu>
2020-03-29 15:54:02 -0500

    Code rearrangement. Decouple train, val and options in the code for modular design.

---
+++ Options.py
@@ -0,0 +1,49 @@
+
+""" argparse configuration""" 
+
+import torch
+import os
+import argparse
+
+
+def get_args():
+    """Get all the args"""
+    parser = argparse.ArgumentParser(description="CS546 Project: Trasnformer Generalization to Arbitary Context Lengths")
+    parser.add_argument(
+            "--epochs",
+            type=int,
+            default='50',
+            help='number of epochs')
+    parser.add_argument(
+            "--train_size",
+            type=int,
+            default='25600',
+            help='number of training examples per epoch')
+    parser.add_argument(
+            "--validation_size",
+            type=int,
+            default='1536',
+            help='number of validation examples')
+    parser.add_argument(
+            "--batch_size",
+            type=int,
+            default='256',
+            help='batch size')
+    parser.add_argument(
+            "--model_size",
+            type=int,
+            default='512',
+            help='internal channel dimension')
+    parser.add_argument(
+            "--digits",
+            type=int,
+            default='5',
+            help='Max number of digits')
+    parser.add_argument(
+            "--seq_type",
+            type=str,
+            choices= ['fib', 'arith', 'palin'],
+            default='fib',
+            help='fib: fibonacci / arith: arithmetic / palin: palindrome')
+
+    return parser.parse_args()
